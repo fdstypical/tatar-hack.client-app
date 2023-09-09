f31f3ac53da8a5070060970bd331aaabba4bb9aa
Andrey Shulmin <ashulmin14@gmail.com>
2023-09-09 21:33:26 +0300

    fix: try fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={classNames}>
-        <NextAuthProvider>{children}</NextAuthProvider>
+        {/* <NextAuthProvider>{children}</NextAuthProvider> */}
+        {children}
       </body>
     </html>
   )
