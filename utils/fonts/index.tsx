17004f303c5ecefcabd220888809e095ff714ad2
Andrey Shulmin <ashulmin14@gmail.com>
2023-09-09 21:25:12 +0300

    fix: try fix

--- utils/fonts/index.tsx
+++ utils/fonts/index.tsx
@@ -1,3 +1,8 @@
-import {Montserrat_Alternates} from 'next/font/google'
+// import { Montserrat_Alternates } from "next/font/google"
 
-export const montserratAlternates = Montserrat_Alternates({subsets: ['cyrillic'], weight: ['100', '200', '300', '400', '500', '600', '700']})
+// export const montserratAlternates = Montserrat_Alternates({
+//   subsets: ["cyrillic"],
+//   weight: ["100", "200", "300", "400", "500", "600", "700"],
+// })
+
+export const montserratAlternates = { className: "" }
